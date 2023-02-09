ff4edb8274fad833f2eac0ef17b7548e8c042dca
Luna <Luna@qq.com>
2023-02-09 16:23:25 +0800

    add gunicorn and uwsgi

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask_script import Manager
-# flask_script的第十五行改一下 from flask_script._compat import text_type
+# flask_script的第十五行改一下 改成from flask_script._compat import text_type
 from flask_migrate import Migrate, MigrateCommand  # 特别注意版本为2.7.0
 from app import create_app
 from app.user.models import User
@@ -20,4 +20,5 @@ migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
 # 脚本形式启动
-manager.run()  # python app.py runserver -h 0.0.0.0 -p 5001
+manager.run()  # python app.py runserver -h 0.0.0.0 -p 5000
+# root/flask_login/venv/lib/python3.11/site-packages/flask_script/__init__.py
